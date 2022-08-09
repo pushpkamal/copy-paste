c2d6828b8bc710b9ae832b3998cf405b834104d1
pushpkamal <88334166+pushpkamal@users.noreply.github.com>
2022-08-09 21:35:36 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,7 @@
+import shutil
+# file path u want to be copied
+src = "C:/Users/Dell/Downloads/pyhton/flower.jpg"
+# file distination u want to be copied 
+dst = "C:/Users/Dell/Downloads/pyhton/folder 2"
+
+shutil.copy2(src,dst)
